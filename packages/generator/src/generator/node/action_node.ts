e01ac57cc74e95db6725825386a285bd5b0b9867
Danpa_cho <danpa725@cau.ac.kr>
2024-01-08 18:31:33 +0900

    feat: add action data node

---
+++ packages/generator/src/generator/node/action_node.ts
@@ -0,0 +1,35 @@
+import { DataNode, type DataNodeChildConstructorOption } from "./data_node.js"
+
+export interface ActionNodeConstructorOptions
+    extends DataNodeChildConstructorOption {
+    type: "button" | "input" | "textarea" | "select" | "option" | "form"
+}
+export class ActionNode extends DataNode {
+    public constructor({
+        text,
+        selector,
+        relevance,
+        type,
+    }: ActionNodeConstructorOptions) {
+        super({
+            text,
+            selector,
+            relevance,
+            node_type: "action",
+        })
+        this.type = type
+    }
+    public type: "button" | "input" | "textarea" | "select" | "option" | "form"
+
+    public static override Tags: string[] = [
+        "button",
+        "input",
+        "textarea",
+        "select",
+        "option",
+        "form",
+    ]
+    public static override is(tagName: string): tagName is ActionNode["type"] {
+        return ActionNode.Tags.includes(tagName)
+    }
+}
