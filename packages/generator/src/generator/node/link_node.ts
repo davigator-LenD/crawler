19e4cc4b7856da1c5f2644ddd97315b6aab50073
Danpa_cho <danpa725@cau.ac.kr>
2024-01-08 18:31:08 +0900

    feat: add link data node

---
+++ packages/generator/src/generator/node/link_node.ts
@@ -0,0 +1,28 @@
+import { DataNode, DataNodeChildConstructorOption } from "./data_node.js"
+
+export interface LinkNodeConstructorOptions
+    extends DataNodeChildConstructorOption {
+    href: string
+}
+export class LinkNode extends DataNode {
+    public readonly href: string
+    public constructor({
+        text,
+        selector,
+        relevance,
+        href,
+    }: LinkNodeConstructorOptions) {
+        super({
+            text,
+            selector,
+            relevance,
+            node_type: "link",
+        })
+        this.href = href
+    }
+
+    public static override Tags: string[] = ["a"]
+    public static override is(tagName: string): tagName is "a" {
+        return LinkNode.Tags.includes(tagName)
+    }
+}
